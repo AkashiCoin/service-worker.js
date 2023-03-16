403ab7b065e7f2906d62bc1f67bfe66ae1e219e9
AkashiCoin <55268546+AkashiCoin@users.noreply.github.com>
2023-03-16 18:51:08 +0800

    fix: pixiv service worker

--- pixiv/service-worker.js
+++ pixiv/service-worker.js
@@ -1,10 +1,10 @@
 // service-worker.js
 self.addEventListener('install', () => {
-	self.skipWaiting()
+    self.skipWaiting()
 })
 
 self.addEventListener('activate', event => {
-	event.waitUntil(self.clients.claim())
+    event.waitUntil(self.clients.claim())
 })
 
 const enterprise = "(function(){var w=window,C='___grecaptcha_cfg',cfg=w[C]=w[C]||{},N='grecaptcha';var E='enterprise',a=w[N]=w[N]||{},gr=a[E]=a[E]||{};gr.ready=gr.ready||function(f){(cfg['fns']=cfg['fns']||[]).push(f);};w['__recaptcha_api']='https://www.recaptcha.net/recaptcha/enterprise/';(cfg['enterprise']=cfg['enterprise']||[]).push(true);(cfg['enterprise2fa']=cfg['enterprise2fa']||[]).push(true);(cfg['render']=cfg['render']||[]).push('6LfF1dcZAAAAAOHQX8v16MX5SktDwmQINVD_6mBF');w['__google_recaptcha_client']=true;var d=document,po=d.createElement('script');po.type='text/javascript';po.async=true;po.src='https://www.gstatic.com/recaptcha/releases/O4xzMiFqEvA4YhWjk5t8Xuas/recaptcha__zh_cn.js';po.crossOrigin='anonymous';var e=d.querySelector('script[nonce]'),n=e&&(e['nonce']||e.getAttribute('nonce'));if(n){po.setAttribute('nonce',n);}var s=d.getElementsByTagName('script')[0];s.parentNode.insertBefore(po, s);})();"
@@ -13,8 +13,7 @@ const crack_rechapcha = async (request) => {
     let url = new URL(request.url)
     if (url.pathname.startsWith('/recaptcha/enterprise/anchor')) {
         url.searchParams.set('co', "aHR0cHM6Ly93d3cucGl4aXYubmV0OjQ0Mw..")
-    }
-    else if (url.pathname.startsWith('/recaptcha/enterprise.js')) {
+    } else if (url.pathname.startsWith('/recaptcha/enterprise.js')) {
         return new Response(enterprise, {
             headers: {
                 'Content-Type': 'application/javascript',
@@ -42,14 +41,42 @@ const baidu_pximg = async (request) => {
     let url = request.url;
     const org_url = new URL(url);
     url = url.replace(/https:\/\/i.pximg.cf\/(.*)([.jpg|.png])/g, "https:\/\/gimg2.baidu.com/image_search/&app=2020&src=i.pximg.cf/$1$2");
-    let response = await fetch(url, {
-        mode: 'cors',
-        ...request
-    });
-    if (response.headers.get('Content-Length') == 52) {
-        return await fetch(org_url, request);
-    }
-    return response
+    
+    const controller = new AbortController();
+
+    const controller2 = new AbortController();
+
+    let task = new Promise(async (resolve, reject) => {
+        fetch(url, {
+            mode: 'cors',
+            signal: controller.signal,
+            ...request
+        }).then(async response => {
+            if (response.headers.get('Content-Length') == 52) {
+                console.log('[Baidu] Image proxy error, use original url: ' + org_url)
+                reject(null);
+            }
+            controller2.abort();
+            resolve(response)
+        }).catch(err => {
+            reject(null)
+        })
+        fetch(org_url, {
+            signal: controller2.signal,
+            ...request
+        }).then(async response => {
+            controller.abort();
+            resolve(response);
+        }).catch(err => {
+            reject(null)
+        })
+    })
+    return task.then(response => {
+        if (response) return response;
+        return fetch(org_url, request);
+    }).catch(err => {
+        return fetch(org_url, request);
+    })
 }
 
 self.onfetch = event => {
